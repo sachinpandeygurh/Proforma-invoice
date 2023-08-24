fbc9d7d176c40ff4e7f21a378c7fd93d64159e1c
sachinpandeygurh <sachingurh@gmail.com>
2023-08-24 23:57:58 +0530

    commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import InvoiceTable from './Table'
 import TotalDisc from './TotalDisc'
+import AddTr from './AddTr'
 
 const Main = () => {
   return (
      <div className='container-xxl'>
     <InvoiceTable/>
-
-  
+    <AddTr/>  
     <TotalDisc/>
     </div>
   )
